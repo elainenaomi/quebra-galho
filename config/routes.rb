5466533c6074137e909cebf991a59d440e7f61e5
Lorrene <lorrenestacy@Lorrenes-MacBook-Pro.local>
2016-12-10 15:12:30 -0200

    pages e routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,7 @@ Rails.application.routes.draw do
   resources :ferramenta
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root :to => "ferramenta#index"
+
+
+  get "/pages/home"
 end
